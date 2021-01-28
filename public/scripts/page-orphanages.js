b56fcac5d3c9dc034c0b1dcc3658056226f3aaa6
Aron <aronreis2@gmail.com>
2021-01-28 16:58:18 +0000

    page-orphanages.js added

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,31 @@
+//Lisbon coordinates in DD (decimal degree): lon 38.71667, lat -9.13333
+const map = L.map("mapid").setView([38.73, -9.15], 13);
+
+//The parameter of the method addTo should be the name of the constant map
+//creates and adds a titleLayer
+L
+.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+.addTo(map);
+
+//creates a icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-market.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// crates a popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+
+//creates and adds a market
+L
+.marker([38.73, -9.15])
+.addTo(map)
+.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+.openPopup();
