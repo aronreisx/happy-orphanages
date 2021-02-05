523df69bd4569eb61289f3f3fe18638fe46399cc
Aron <aronreis2@gmail.com>
2021-02-05 19:31:22 +0000

    server js modified

--- src/server.js
+++ src/server.js
@@ -1,10 +1,29 @@
 //importing express lib
 const express = require('express')
+
+//new lib for resolve different path's OS problems
+const path = require('path')
+
+//express function as a constant
 const server = express()
 
+//calling function inside const
+server
+
+//function for using static files
+.use(express.static('public'))
+
+//setting template engine up
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'hbs')
+
 //creating a route
-server.get('/', (request, response) => {
-    return response.send('Hello World')
+.get('/', (request, response) => {
+    //Tradicional way(that will no be used): sending the html to browser when resquested
+    /* return response.sendFile(path.join(__dirname, 'views', 'index.html')) */
+    
+    //Set response as a hbs's rendering
+    return response.render('index')
 })
 
 //starting server at specified port
