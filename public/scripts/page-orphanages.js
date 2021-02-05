e5d3cf53422a69b2cb934608bdfaac6c722fff68
Aron <aronreis2@gmail.com>
2021-02-05 23:00:56 +0000

    script files modified

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -7,7 +7,7 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
 
 //creates a icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
@@ -19,7 +19,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>')
 
 //creates and adds a market
 L.marker([38.73, -9.15], { icon })
