0c38d09af2922046e2e7547c13e82fa3cea1a849
Aron <aronreis2@gmail.com>
2021-02-05 22:58:57 +0000

    pages js added

---
+++ src/pages.js
@@ -0,0 +1,14 @@
+module.exports = {
+    index(req, res) {
+        return res.render('index')
+    },
+    orphanage(req, res) {
+        return res.render('orphanage')
+    },
+    orphanages(req, res) {
+        return res.render('orphanages')
+    },
+    createOrphanage(req, res) {
+        return res.render('create-orphanage')
+    }
+}
