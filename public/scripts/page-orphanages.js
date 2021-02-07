7e05b1a166ee904cef924094422b208ec08c4c92
Aron <aronreis2@gmail.com>
2021-02-07 23:08:05 +0000

    page-orphanages js modified

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -13,15 +13,32 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
-// crates a popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="/orphanage?id=1" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>')
+//a function that adds a marker and has a destructuring operator as parameter for the object that it will recieve
+function addMarker({id, name, lat, lng}){
+    // crates a popup overlay
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map-popup',
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>`)
 
-//creates and adds a market
-L.marker([38.73, -9.15], { icon })
-.addTo(map)
-.bindPopup(popup);
+    //creates and adds a market
+    L.marker([lat, lng], { icon })
+    .addTo(map)
+    .bindPopup(popup);
+}
+
+//getting spans from div orphanages on html
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+//looping each span to extract information, generate objects, using each object information on function add marker
+orphanagesSpan.forEach(span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng        
+    }
+    addMarker(orphanage)
+})
